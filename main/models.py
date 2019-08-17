9ed10228f08c3cd4e8fd1d4dbbfc0bbb5987a763
Tushar <tushar.mohan2001@gmail.com>
2019-08-17 16:54:25 +0530

    gunicorn

--- main/models.py
+++ main/models.py
@@ -2,10 +2,17 @@ from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.contrib.auth.models import User
 
+
 # Create your models here.
 class File(models.Model):
     name=models.CharField(max_length=256)
     path=models.FileField()
-    
+  #  user= models.ForeignKey('User', on_delete=models.CASCADE)
     def __str__(self):
         return self.name
+'''
+class User(models.Model):
+    name=User.first_name
+    username=User.username
+    def __str__(self):
+        return self.name '''
